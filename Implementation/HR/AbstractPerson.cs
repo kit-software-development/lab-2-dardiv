74e01535644f123482144c6bacfb504e81f6a3d6
Daria Divenkova <47813973+dardiv@users.noreply.github.com>
2019-03-29 15:42:06 +0300

    Changed all Implementation.HR files

--- Implementation/HR/AbstractPerson.cs
+++ Implementation/HR/AbstractPerson.cs
@@ -1,39 +1,39 @@
-﻿namespace Practice.HR
+﻿using Practice.HR.Events;
+using Practice.Common;
+using System;
+
+namespace Practice.HR
 {
     /// <summary>
     ///     Абстрактная база для описания конкретных реализаций типа "Человек".
     ///     Используется для дальнейшего наследования.
     /// </summary>
-    internal abstract class AbstractPerson
+    internal abstract class AbstractPerson : IPerson
     {
-        internal Organization.Department Department
-        {
-            get
-            {
-                throw new System.NotImplementedException();
-            }
 
-            set
-            {
-                throw new System.NotImplementedException();
-            }
-        }
-
-        internal Common.Name Name
-        {
-            get
-            {
-                throw new System.NotImplementedException();
-            }
-
-            set
-            {
-                throw new System.NotImplementedException();
-            }
-        }
-        
         /*
          * TODO #3: Реализуйте интерфейс IPerson для класса AbstractPerson
          */
+
+        private Name name;
+
+        public IName Name
+        {
+            get { return name; }
+            set
+            {
+                ValueChangeEventArgs<IName> args = new ValueChangeEventArgs<IName>(name);
+                
+                name = new Name(value);
+
+                if (NameChange != null)
+                {
+                    NameChange(this, args);
+                }
+            }
+        }
+
+        public event EventHandler<ValueChangeEventArgs<IName>> NameChange;
+
     }
 }
--- Implementation/HR/Employee.cs
+++ Implementation/HR/Employee.cs
@@ -1,12 +1,36 @@
-﻿namespace Practice.HR
+﻿using Practice.HR.Events;
+using Practice.Organization;
+using System;
+
+namespace Practice.HR
 {
     /// <summary>
     ///     Скрытая реализация представления о сотруднике.
     /// </summary>
-    internal class Employee : AbstractPerson
+    internal class Employee : AbstractPerson, IEmployee
     {
         /*
          * TODO #5: Реализуйте интерфейс IEmployee для класса Employee
          */
+
+        private Department department;
+
+        public IDepartment Department
+        {
+            get { return department; }
+            set
+            {
+                ValueChangeEventArgs<IDepartment> args = new ValueChangeEventArgs<IDepartment>(Department);
+                
+                department = new Department(value);
+
+                if (DepartmentChange != null)
+                {
+                    DepartmentChange(this, args);
+                }
+            }
+        }
+
+        public event EventHandler<ValueChangeEventArgs<IDepartment>> DepartmentChange;
     }
 }
