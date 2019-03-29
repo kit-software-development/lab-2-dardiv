48b4b580b8f979e1086019b0c7e96f338d09e8a8
Daria Divenkova <47813973+dardiv@users.noreply.github.com>
2019-03-29 15:40:28 +0300

    Changed Name.cs

--- Implementation/Common/Name.cs
+++ Implementation/Common/Name.cs
@@ -3,26 +3,59 @@
     /// <summary>
     ///     Скрытая реализация представления об имени человека.
     /// </summary>
-    internal struct Name
+    public struct Name : IName
     {
         /*
          * TODO #1: Реализуйте интерфейс IName для структуры Name
          */
+        /// <summary>
+        ///     Полная форма имени.
+        /// </summary>
+        public string FullName { get; }
+        /// <summary>
+        ///     Короткая форма имени.
+        /// </summary>
+        public string ShortName { get; }
 
         /// <summary>
         ///     Имя.
         /// </summary>
         public string FirstName { get; }
-        
+
         /// <summary>
         ///     Фамилия.
         /// </summary>
         public string Surname { get; }
-        
+
         /// <summary>
         ///     Отчество.
         /// </summary>
         public string Patronymic { get; }
-        
+
+        public Name(IName name)
+        {
+            FullName = name.FullName;
+            ShortName = name.ShortName;
+            
+            int SurnameLength = FullName.IndexOf(' ');
+            int FirstnameLength = FullName.Substring(SurnameLength + 1).IndexOf(' ');
+            
+            
+            Surname = FullName.Substring(0, SurnameLength + 1);
+            FirstName = FullName.Substring(SurnameLength + 1, FirstnameLength);
+            Patronymic = FullName.Substring(FirstnameLength + 1);
+        }
+
+        public Name(string surname, string firstname, string patronymic)
+        {
+            Surname = surname;
+            FirstName = firstname;
+            Patronymic = patronymic;
+            FullName = surname + ' ' + firstname + ' ' + patronymic;
+            ShortName = surname + ' ' + firstname[0] + ". " + patronymic[0] + '.';
+        }
+
+
+
     }
 }
